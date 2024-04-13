0e83e11778f101771f2f6724c3f7ead2d5c95082
21120447 <haonhat2729@gmail.com>
2024-04-14 01:08:22 +0700

    done banner

---
+++ frontend/src/components/ProductFrame.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductFrame = () => {
+  return (
+    <div>ProductFrame</div>
+  )
+}
+
+export default ProductFrame
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import CarouselCustom from '../components/CarouselCustom'
+import banner from '../assets/images/banners/blackFriday.gif'
+import ProductFrame from '../components/ProductFrame'
 
 const Home = () => {
   return (
     <div>
       <CarouselCustom/>
+      <div className='w-full mt-6 mb-10 flex justify-center'>
+        <img src={banner}/>
+      </div>
+      <div>
+        <ProductFrame/>
+      </div>
     </div>
   )
 }
