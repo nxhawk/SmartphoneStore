a110319d92ac242360e48e201a327b9b56f79ee7
21120447 <haonhat2729@gmail.com>
2024-04-15 23:11:58 +0700

    fix padding marquee reponsive

--- frontend/src/components/CustomMarquee.tsx
+++ frontend/src/components/CustomMarquee.tsx
@@ -2,8 +2,10 @@ import Marquee from "react-fast-marquee";
 
 const CustomMarquee = ({ message }: {message: string}) => {
   return (
-    <div className="bg-blue-200 py-3 shadow-inner rounded-sm">
-      <Marquee className="font-bold text-xl tracking-wide">{ message }</Marquee>
+    <div className="px-1">
+      <div className="bg-blue-200 py-3 shadow-inner rounded-sm">
+        <Marquee className="font-bold text-xl tracking-wide">{ message }</Marquee>
+      </div>
     </div>
   )
 }
